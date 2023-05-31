b2cb009e4b1577529498dc7390d0780715a3fe79
Gunter Reitberger <reitberger@bluebridge.de>
2023-05-31 09:53:52 +0200

    initial checkin

---
+++ RERCore.Common/EventReceivers/ProcessEventResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization;
+
+namespace Microsoft.SharePoint.Client.EventReceivers
+{
+    [DataContract(Name = "ProcessEventResponse", Namespace = "http://schemas.microsoft.com/sharepoint/remoteapp/")]
+    public class ProcessEventResponse
+    {
+        [DataMember]
+        public SPRemoteEventResult ProcessEventResult { get; set; }
+    }
+}
